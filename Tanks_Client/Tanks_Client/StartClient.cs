68b0d6e9b5a3278ead1a134ff119f77d815e783b
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2015-10-25 20:29:52 +0530

    join button working-s

--- Tanks_Client/Tanks_Client/StartClient.cs
+++ Tanks_Client/Tanks_Client/StartClient.cs
@@ -23,7 +23,7 @@ namespace Tanks_Client
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.Run(new UI.ClientUI());
         }
     }
 }
--- Tanks_Client/Tanks_Client/UI/ClientUI.cs
+++ Tanks_Client/Tanks_Client/UI/ClientUI.cs
@@ -12,9 +12,15 @@ namespace Tanks_Client.UI
 {
     public partial class ClientUI : Form
     {
+        private ClientClass networkClient;
         public ClientUI()
         {
             InitializeComponent();
+
+            //initialize grid
+
+            //instantiate network client
+            networkClient = new ClientClass();
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -32,9 +38,11 @@ namespace Tanks_Client.UI
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
+
+        private void JoinGameButton_Click(object sender, EventArgs e)
+        {
+            networkClient.Sender("JOIN#");
         }
 
         private void button3_Click(object sender, EventArgs e)
