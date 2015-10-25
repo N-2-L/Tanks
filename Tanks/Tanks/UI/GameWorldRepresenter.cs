3cab9de9f8930f029ce9bd3a8757de0a7bd7e281
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2015-10-25 18:34:05 +0530

    -s

---
+++ Tanks/Tanks/UI/GameWorldRepresenter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tanks.UI
+{
+    class GameWorldRepresenter
+    {
+        // GUI controller
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TanksUI());
+        }
+    }
+}
